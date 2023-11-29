33e214426bd6dca69cbc57833f536c5f798c9193
Vanessa Trajano <vanessa.trajano@estudante.ufjf.br>
2023-11-29 19:54:22 -0300

    Cria consts no main.js

--- main.js
+++ main.js
@@ -2,3 +2,5 @@
 
 const pEntrada = document.querySelector('#entrada')
 const olSaida = document.querySelector('#itens')
+const btnAdicionar = document.querySelector('#adicionar')
+const btnLimpar = document.querySelector('#limpar')
